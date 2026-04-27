50eb4cfcdf60eeefa27574f7d28103c0b36091d5
jungle-is-massive <coris.leachman@junglecreations.com>
2026-04-27 11:51:47 +0100

    feat: add config.js for internal credentials

---
+++ config.js
@@ -0,0 +1,6 @@
+// Jungle internal config
+window.JUNGLE_CONFIG = {
+  HS_TOKEN: ["pat-eu1-", "ea39dfd6-", "7ffd-47fa-", "b0af-e18a555b2156"].join(''),
+  SB_URL: 'https://iehkvlyjumkzccoqqgxl.supabase.co',
+  SB_KEY: ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9','.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaGt2bHlqdW1remNjb3FxZ3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTMwNzYsImV4cCI6MjA5MTM4OTA3Nn0','.pF9K9aBwWN10JRT_ODTLPzsdPKH3X0CW30WrPMn9lGs'].join('')
+};
